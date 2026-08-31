fc1c5ac8a911ec2eec87de588a4b2ca3be6c0896
Seriki Walter Yakub <251400999+auraecosystem@users.noreply.github.com>
2026-08-31 17:42:09 +0100

    Add multiple installation options for mypy

--- bash.zsh
+++ bash.zsh
@@ -1,3 +1,6 @@
 python3 -m pip install -U git+https://github.com/python/mypy.git
 # or if you don't have 'git' installed
 python3 -m pip install -U https://github.com/python/mypy/zipball/main
+python3 -m pip install -U https://github.com/python/mypy/archive/refs/heads/main.zip
+python3 -m pip install -U git+https://github.com/python/mypy.git
+python3 -m pip install -U https://github.com/python/mypy/archive/main.zip
